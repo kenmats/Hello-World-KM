f78ef569f6993814ff18e67a5ea3e6bf1616b3f7
kenmats <kenmats@users.noreply.github.com>
2019-11-19 14:26:33 -0600

    lmao
    owned

--- HelloWorldDS/src/helloworldds/HelloWorldDS.java
+++ HelloWorldDS/src/helloworldds/HelloWorldDS.java
@@ -4,10 +4,7 @@ package helloworldds;
 public class HelloWorldDS {
 
     public static void main(String[] args) {
-        
-        System.out.println("Hello World");
-    for (int i = 1; i < 11; i++){
-        System.out.println(i);
+        //owned lol
     }
     }
     
